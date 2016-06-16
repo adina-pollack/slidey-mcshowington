156f45a930db5c7b9a55f2d997e491d0d82918f4
Adina Pollack <adinap16@gmail.com>
2016-06-16 16:56:44 -0400

    working search function

--- slidey_js.js
+++ slidey_js.js
@@ -1,23 +1,34 @@
-$(document).ready( function() {
-  $("#inputs").on("keydown", function(e){
-    if(e.keyCode === 13){
-      var searchTerm = $("input#word").val();
-      var searchURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&rating=pg&limit=10&q="+searchTerm;
-      $.ajax({
-        url: searchURL,
-        type: "GET",
-        dataType: "json"
-      }).done(function(response){
-        console.log(response);
-        var image = response.data[0].images.original.url;
-        console.log(image);
-        $("#background").css("background-image", "url('" + image + "')")
+$(document).ready( function(){
+    $("#inputs").on("keydown", function(e){
+      if(e.keyCode === 13){
+        var searchTerm = $("#word").val();
+        var searchURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&rating=pg&limit=10&q="+searchTerm;
+        $.ajax({
+          url: searchURL,
+          type: "GET",
+          dataType: "json",
+          cache: false
+        }).done(function(response){
+          var counter = 0
+          var newSearch;
+          function makeBackground(){
+            if (counter < response.data.length){
+              var image = response.data[counter].images.original.url;
+              $("#background").css("background-image", "url('" + image + "')")
+              console.log(image);
+              counter++
+            }
+          }
+          makeBackground();
+          gifTimer = setInterval(makeBackground, 5000)
 
-      }).fail(function(response){
-        console.log("Nah girl!")
-      });
-    }
-  });
+
+        }).fail(function(response){
+          console.log("Nah girl!")
+        });
+      }
+    });
+
 
 
 
